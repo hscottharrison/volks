fa61632aa4df2f837215f73b2c8b42a4b0d64f48
hscottharrison <h.scott.harrison@tcu.edu>
2017-06-30 10:39:33 -0500

    added wave effect to nav bar

--- js/jq.js
+++
@@ -1,7 +0,0 @@
-angular.element(document).ready(function(){
-  angular.element('.down-arrow').click(function() {
-    $('html, body').animate({
-        scrollTop: $("about").offset().top
-    }, 10000);
-});
-})
