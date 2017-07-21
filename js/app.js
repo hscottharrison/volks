dccd985c8eb3db156dc77f82e29959ee7cf84dcb
hscottharrison <h.scott.harrison@tcu.edu>
2017-07-20 22:32:33 -0500

    set up views for each service

--- js/app.js
+++ js/app.js
@@ -16,4 +16,20 @@ angular.module('volks', ['ui.router', 'ui.materialize', 'angularTypewrite'])
     url: '/managedservices',
     templateUrl: '../views/services.html'
   })
+  .state('telecom', {
+    url: '/managedservices/telecom',
+    templateUrl: '../views/services/telecom.html'
+  })
+  .state('it', {
+    url: '/managedservices/it',
+    templateUrl: '../views/services/it.html'
+  })
+  .state('cybersecurity', {
+    url: '/managedservices/cybersecurity',
+    templateUrl: '../views/services/cybersecurity.html'
+  })
+  .state('big_data', {
+    url: '/managedservices/big_data',
+    templateUrl: '../views/services/big_data.html'
+  })
 })
